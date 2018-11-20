cbf95e1a7e56d68a4f185e6587fb94bd486703b3
Gabriel Chung <gabrielchung1128@gmail.com>
2018-11-20 00:11:13 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,13 +1,7 @@
-const data =
+var data = 
 [
-  "This"
-  ,
-  "is"
-  ,
-  "a"
-  ,
-  "test."
-  ,
-  "yeah!"
+     ["area"   , "usecase", "type"   , "name"  , "link"              , "tags"]
+    ,["General", "search" , "website", "Google", "https://google.com", []    ]
+    ,["General", "search" , "website", "Bing"  , "https://bing.com"  , []    ]
+    ,["General", "portal" , "website", "Yahoo" , "https://yahoo.com" , []    ]
 ]
-;
